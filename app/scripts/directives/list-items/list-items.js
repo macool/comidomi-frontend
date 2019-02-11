f62bb83a45888936376cdcd71c08aba0be08c7a6
Lenin Capa <leviskp87@gmail.com>
2019-02-11 01:11:44 -0500

    redesign cart view

---
+++ app/scripts/directives/list-items/list-items.js
@@ -0,0 +1,26 @@
+(function () {
+  'use strict';
+  /* jshint validthis:true */
+  angular
+    .module('porttare.directives')
+    .directive('listItems', slickItem);
+
+  function slickItem() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'templates/directives/list-items/list-items.html',
+      scope: {
+        data: '=',
+        onClick: '&'
+      },
+      controller: listItemsController,
+      controllerAs: 'liVm',
+      bindToController: true
+    };
+
+    return directive;
+
+    function listItemsController() {
+    }
+  }
+})();
