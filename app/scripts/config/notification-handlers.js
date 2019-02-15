a1f54e94d32839f9a28daa1f3b24fb34859596d5
Mario Andrés Correa <a@macool.me>
2019-02-15 18:07:49 -0500

    notification handlers

--- app/scripts/config/notification-handlers.js
+++ app/scripts/config/notification-handlers.js
@@ -7,22 +7,28 @@
 
   function notificationHandlersConfig($state,
                                       $rootScope){
-    // var handlers = {
-    //   'provider_new_order': providerNewOrder
-    // };
-    //
-    // $rootScope.$on('porttare:notification', function(e, data){
-    //   // TODO refactor
-    //   // if (data.additionalData.handler) {
-    //   //   var handler = handlers[data.additionalData.handler];
-    //   //   handler.call(this, data);
-    //   // }
-    // });
-    //
-    // function providerNewOrder(data){
-    //   $state.go('provider.orders.show', {
-    //     id: data.additionalData.order_id // jshint ignore:line
-    //   });
-    // }
+    var handlers = {
+      'provider_new_order': providerNewOrder,
+      'new_shipping_request': newShippingRequest
+    };
+
+    $rootScope.$on('porttare:notification', function(e, data){
+      if (data.notification_handler) { // jshint ignore:line
+        var handler = handlers[data.notification_handler]; // jshint ignore:line
+        handler.call(this, data);
+      }
+    });
+
+    function providerNewOrder(data){
+      $state.go('provider.orders.show', {
+        id: data.additionalData.order_id
+      });
+    }
+
+    function newShippingRequest(data){
+      if (data.wasTapped) {
+        $state.go('courier.orders');
+      }
+    }
   }
 })();
