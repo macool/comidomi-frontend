48269294948801d86f017e050939317e1bdc4f86
Lenin Capa <leviskp87@gmail.com>
2019-03-02 12:44:40 -0500

    add colors by status order and show user info

--- app/scripts/controllers/courier/shippings.js
+++ app/scripts/controllers/courier/shippings.js
@@ -8,14 +8,25 @@
   function ShippingController(shippingMyRequests) {
 
     var shVm = this;
+
+    var ICONS = {
+      assigned: 'directions_bike',
+      delivered: 'check_circle',
+      in_progress: 'person_pin'
+    };
     
     shVm.totalOrders = 0;
+    shVm.getIconStatus = getIconStatus;
 
     init();
 
     function init() {
       shVm.orders = shippingMyRequests;
       shVm.totalOrders = shVm.orders.length;
+    }
+
+    function getIconStatus(order){
+      return ICONS[order.status];
     }
   }
 })();
