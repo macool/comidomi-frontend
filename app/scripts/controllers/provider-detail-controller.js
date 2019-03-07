04f48e1295a8350204a9fcabd10f74d0a3716984
Mario Andrés Correa <a@macool.me>
2019-03-07 11:30:54 -0500

    fix bug for back button

--- app/scripts/controllers/provider-detail-controller.js
+++ app/scripts/controllers/provider-detail-controller.js
@@ -5,7 +5,10 @@
     .module('porttare.controllers')
     .controller('ProviderDetailController', ProviderDetailController);
 
-  function ProviderDetailController(data, CommonService) {
+  function ProviderDetailController($scope,
+                                    $ionicLoading,
+                                    data,
+                                    CommonService) {
     var providerDetVm = this;
     providerDetVm.provider = data.provider_profile; //jshint ignore:line
     var office = providerDetVm.provider.provider_offices[0]; //jshint ignore:line
@@ -13,5 +16,13 @@
     providerDetVm.closedMsgTranslation = {
       providerName: providerDetVm.provider.nombre_establecimiento //jshint ignore:line
     };
+
+    // HACK:
+    // fixes a bug where transitioning (back) to this
+    // screen using the hardware back button gets stuck
+    // on $ionicLoading
+    $scope.$on('$ionicView.enter', function(){
+      $ionicLoading.hide();
+    });
   }
 })();
