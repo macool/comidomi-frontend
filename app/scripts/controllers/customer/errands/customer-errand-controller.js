395585df17dc29210fce17702a5baba7fb95a055
Lenin Capa <lenin.capa@hpe.com>
2019-03-30 17:46:14 -0500

    validate phone number and name before to send errand

--- app/scripts/controllers/customer/errands/customer-errand-controller.js
+++ app/scripts/controllers/customer/errands/customer-errand-controller.js
@@ -6,29 +6,52 @@
     .controller('CustomerErrandController', CustomerErrandController);
 
   function CustomerErrandController($scope,
-    ModalService,
-    $auth,
-    $state,
-    customerAddresses,
-    ErrandsService,
-    $ionicLoading,
-    $ionicPopup,
-    CommonService) {
-    var errVm = this,
-      addresss = customerAddresses;
+                                    ModalService,
+                                    $auth,
+                                    $state,
+                                    customerAddresses,
+                                    ErrandsService,
+                                    $ionicLoading,
+                                    $ionicPopup,
+                                    CommonService,
+                                    ProfileAddressesService,
+                                    $ionicScrollDelegate,
+                                    ProfileService) {
 
-    errVm.selecAddress = showAddressesModal;
+    var errVm = this;
+
+    errVm.addresses = customerAddresses || [];
+    errVm.selectAddress = selectAddressesModal;
     errVm.user = $auth.user;
     errVm.indexSelected = null;
     errVm.errand = {};
     errVm.submitProcess = submitProcess;
+    errVm.newAddress = newAddress;
 
-    function showAddressesModal() {
+    var inputSelectOptions = {
+      0: {
+        onClick: newAddress,
+        text: 'Agregar Dirección',
+        address: null
+      },
+      1: {
+        onClick: function(){},
+        address: errVm.addresses[0]
+      },
+      default: {
+        onClick: selectAddressesModal,
+        text: 'Seleccionar Dirección',
+        address: null
+      }
+    };
+
+    errVm.inputOptions = inputSelectOptions[errVm.addresses.length] || inputSelectOptions.default;
+
+    function selectAddressesModal() {
       $scope.vm = {
-        user: errVm.user,
-        addresses: addresss,
+        addresses: errVm.addresses,
         clickAddress: clickAddress,
-        indexSelected: errVm.indexSelected,
+        addressSelected: errVm.inputOptions.address,
         addAddress: addAddress,
         closeModal: closeModal
       };
@@ -46,36 +69,103 @@
 
     function resetValues() {
       errVm.errand = {};
-      errVm.addressSelected = null;
       errVm.indexSelected = null;
     }
 
     function clickAddress(address, index) {
-      $scope.vm.indexSelected = index;
-      errVm.indexSelected = index;
-    };
+      $scope.vm.addressSelected = address;
+    }
 
-    function addAddress() {
-      errVm.addressSelected = customerAddresses[errVm.indexSelected];
-      return ModalService.closeModal();
-    };
+    function addAddress(address) {
+      errVm.inputOptions.address = address;
+      closeModal();
+    }
+
+    function newAddress(){
+      closeModal().then(function(){
+        $scope.pfaVm = {
+          closeModal: closeModal,
+          processAddress: saveNewAddress,
+          defaultInCurrentGeolocation: true
+        };
+        ModalService.showModal({
+          parentScope: $scope,
+          fromTemplateUrl: 'templates/profile/addresses/modal-form.html'
+        });
+      });
+    }
+
+    function saveNewAddress(data){
+      ProfileAddressesService.createAddresses(data).then(function(response){
+        var newCustomerAddress = response.customer_address; //jshint ignore:line
+        errVm.addresses.push(newCustomerAddress);
+        errVm.inputOptions.address = newCustomerAddress;
+        closeModal();
+      }, function(error){
+        $scope.pfaVm.messages = error.errors;
+      });
+    }
 
     function submitProcess() {
+      if (needsToAddPhoneNumberOrName()){
+        closeModal().then(function(){
+          $scope.userAddPhone = {
+            closeModal: closeModal,
+            submitModal: addPhoneNumber,
+            updateProfileForm: {},
+            userEdit: angular.copy(errVm.user),
+            scrollToBottom: scrollCustomerProfileFormToBottom
+          };
+          ModalService.showModal({
+            parentScope: $scope,
+            fromTemplateUrl: 'templates/cart/add-phone-number.html',
+          });
+        });
+      }else {
+        sendErrand();
+      }
+    }
+
+    function scrollCustomerProfileFormToBottom(){
+      $ionicScrollDelegate.$getByHandle('customer-profile-form-scroll').scrollBottom(true);
+    }
+
+    function needsToAddPhoneNumberOrName(){
+      return !errVm.user.phone_number || !errVm.user.name;  //jshint ignore:line
+    }
+
+    function addPhoneNumber(user) {
+      $ionicLoading.show({
+        template: '{{::("globals.updating"|translate)}}'
+      });
+
+      ProfileService.editProfile(user)
+        .then()
+        .finally(function () {
+          $ionicLoading.hide().then(function(){
+            closeModal().then(function(){
+              sendErrand();
+            });
+          });
+        });
+    }
+
+    function sendErrand() {
       var errandParams = {
-        customer_address_id: errVm.addressSelected.id,
+        customer_address_id: errVm.inputOptions.address.id,  //jshint ignore:line
         description: errVm.errand.description
       };
       $ionicLoading.show({
         template: '{{::("globals.loading"|translate)}}'
       });
       ErrandsService.sendErrand(errandParams)
-        .then(function success(response) {
+        .then(function success() {
           CommonService.nextViewIsRoot();
           $state.go('app.services.providers').then(function() {
             closeModal();
             resetValues();
           });
-        }, function error(res) {
+        }, function error() {
           $ionicLoading.hide();
           var message = '{{::("globals.pleaseTryAgain"|translate)}}';
           $ionicPopup.alert({
